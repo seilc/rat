4329ad38da0a48a99cbc5870414b3757c753b0cb
Seil Weiss <32021834+seilc@users.noreply.github.com>
2026-09-04 00:24:05 -0500

    Update decompctx.py with new include paths

--- tools/decompctx.py
+++ tools/decompctx.py
@@ -19,18 +19,19 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(script_dir, ".."))
 src_dir = os.path.join(root_dir, "src")
 include_dirs = [
-    os.path.join(root_dir, "include"),
-    os.path.join(root_dir, "include/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common/Include"),
-    #os.path.join(root_dir, "include/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common_Embedded/Include"),
-    #os.path.join(root_dir, "include/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common_Embedded/Math/Include"),
-    #os.path.join(root_dir, "include/PowerPC_EABI_Support/MSL/MSL_C/PPC_EABI/Include"),
-    os.path.join(root_dir, "include/PowerPC_EABI_Support/MSL/MSL_C++/MSL_Common/Include"),
+    os.path.join(root_dir, "src"),
+    os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common/Include"),
+    #os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common_Embedded/Include"),
+    #os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common_Embedded/Math/Include"),
+    #os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/PPC_EABI/Include"),
+    os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C++/MSL_Common/Include"),
+    os.path.join(root_dir, "src/rt"),
     os.path.join(root_dir, "src/rt/Engine/Core/gc"),
     os.path.join(root_dir, "src/rt/Engine/Core/x"),
     os.path.join(root_dir, "src/rt/Engine/Game"),
     os.path.join(root_dir, "src/rwsdk/include/gcn"),
-    os.path.join(root_dir, "include/dolphin"),
-    os.path.join(root_dir, "include/bink"),
+    os.path.join(root_dir, "src/dolphin"),
+    os.path.join(root_dir, "src/bink"),
     # Add additional include directories here
 ]
 
